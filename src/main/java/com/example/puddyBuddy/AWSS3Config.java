c398862b839e923d234afe3a32c99528dd66d129
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-10-30 01:54:30 +0900

    s3 연결, 사진 업로드 api 제작

--- src/main/java/com/example/puddyBuddy/AWSS3Config.java
+++ src/main/java/com/example/puddyBuddy/AWSS3Config.java
@@ -1,26 +1,36 @@
 package com.example.puddyBuddy;
+
+
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
-@Configuration
+
 @Service
 @PropertySource("classpath:application-security.yml")
 @NoArgsConstructor
 public class AWSS3Config {
     private AmazonS3 s3Client;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        // AmazonS3Client의 구성을 설정하십시오.
+        return new AmazonS3Client();
+    }
 
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
@@ -34,7 +44,7 @@ public class AWSS3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Bean
+    @PostConstruct
     public void setS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
 
--- src/main/java/com/example/puddyBuddy/service/S3Uploader.java
+++ src/main/java/com/example/puddyBuddy/service/S3Uploader.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -17,13 +18,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
 
-@Slf4j
-@RequiredArgsConstructor    // final 멤버변수가 있으면 생성자 항목에 포함시킴
+@Slf4j  // final 멤버변수가 있으면 생성자 항목에 포함시킴
 @Component
 @Service
 public class S3Uploader {
 
     private final AmazonS3Client amazonS3Client;
+    @Autowired
+    public S3Uploader(AmazonS3Client amazonS3Client) {
+        this.amazonS3Client = amazonS3Client;
+    }
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
